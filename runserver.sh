1bad68fdf43d7e71efffa8e154e90bbe05de2a44
Andriy Voznyuk <andreyv@ukr.net>
2023-05-17 22:31:43 +0200

    Update runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -4,5 +4,5 @@ python manage.py makemigrations --settings=core.settings.production
 python manage.py migrate --settings=core.settings.production
 python manage.py collectstatic --settings=core.settings.production --no-input --clear
 python manage.py update_index --settings=core.settings.production
-gunicorn core.wsgi:application -b :8080  --workers=5   --timeout=190 --graceful-timeout=100 --log-level=DEBUG
+gunicorn core.wsgi:application -b :8082  --workers=5   --timeout=190 --graceful-timeout=100 --log-level=DEBUG
 exec "$@"
